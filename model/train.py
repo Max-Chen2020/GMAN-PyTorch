08adff8c3e27b26f541750a0fef037c935c3381c
Haoyang Chen <63090484+Max-Chen2020@users.noreply.github.com>
2023-08-01 20:05:26 +1000

    Update train.py

--- model/train.py
+++ model/train.py
@@ -67,8 +67,8 @@ def train(device, model, args, log, loss_criterion, optimizer, scheduler):
             v = pred[:, :, :, 0].clone()
             q = pred[:, :, :, 1].clone()
             k = torch.div(q, v)
-            dqdx = torch.autograd.grad(q, SE, grad_outputs=torch.ones_like(q)) 
-            dkdt = torch.autograd.grad(k, TE, grad_outputs=torch.ones_like(q)) 
+            dqdx = torch.autograd.grad(q, SE, grad_outputs=torch.ones_like(q), is_grads_batched=True) 
+            dkdt = torch.autograd.grad(k, TE, grad_outputs=torch.ones_like(q), is_grads_batched=True) 
             print(f' using grad dqdx1: {dqdx.shape} \n using grad dkdt: {dkdt.shape}')
             loss_batch = loss_criterion(pred, label)
             # loss_batch = dl_loss + phy_loss
