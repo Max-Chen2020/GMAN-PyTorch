6b6db24eaba1b504751edd6bd5760a224e11dc7e
Haoyang Chen <63090484+Max-Chen2020@users.noreply.github.com>
2023-08-30 17:37:44 +1000

    Update model_.py

--- model/model_.py
+++ model/model_.py
@@ -330,9 +330,10 @@ class gatedFusion(nn.Module):
         XS = self.FC_xs(HS)
         XT = self.FC_xt(HT)
         XP = self.FC_xp(HP)
-        H = torch.add(torch.add(XS, XT), XP)
-        w = torch.softmax(H)
-        H = torch.mul(w, H)
+        x = torch.sigmoid(torch.add(XS, XT))
+        y = torch.sigmoid(torch.add(XS, XP))
+        z = torch.sigmoid(torch.add(XT, XP))
+        H = torch.add(torch.add(torch.mul(x+y, HS), torch.mul(x+z, HT)), torch.mul(y+z, HP))
         H = self.FC_h(H)
         del XS, XT, XP, w
         return H
