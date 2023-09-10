e9739e931ab5a5bb1647fc53673242e7125e1976
Haoyang Chen <63090484+Max-Chen2020@users.noreply.github.com>
2023-09-10 20:07:31 +1000

    Update utils_.py

--- utils/utils_.py
+++ utils/utils_.py
@@ -50,7 +50,7 @@ def load_data(args):
     # Traffic
     speed = pd.read_hdf(args.traffic_file, key = 'speed', mode = 'r')
     flow = pd.read_hdf(args.traffic_file, key = 'flow', mode = 'r')
-    traffic = np.stack((speed.values, flow.values), axis = -1)
+    traffic = np.stack((flow.values, speed.values), axis = -1)
     traffic = torch.from_numpy(traffic)
     # train/val/test
     num_step = speed.shape[0]
